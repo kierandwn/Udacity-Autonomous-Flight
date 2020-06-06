1c0b4b718c6d756eb41a070ddc16e1e1c0ba0bea
kierandwn <kieran.downie@outlook.com>
2020-06-06 15:41:18 +0100

    added diagnonal movements to valid_actions; added own implementation of euclideanas recommended on forum

--- planning_utils.py
+++ planning_utils.py
@@ -56,6 +56,12 @@ class Action(Enum):
     NORTH = (-1, 0, 1)
     SOUTH = (1, 0, 1)
 
+    # DIAGONALS
+    NORTH_WEST = (-1, -1, np.sqrt(2))
+    NORTH_EAST = (-1, 1, np.sqrt(2))
+    SOUTH_WEST = (1, -1, np.sqrt(2))
+    SOUTH_EAST = (1, 1, np.sqrt(2))
+
     @property
     def cost(self):
         return self.value[2]
@@ -76,15 +82,24 @@ def valid_actions(grid, current_node):
     # check if the node is off the grid or
     # it's an obstacle
 
-    if x - 1 < 0 or grid[x - 1, y] == 1:
+    if x - 1 < 0 or grid[x-1, y] == 1:
         valid_actions.remove(Action.NORTH)
-    if x + 1 > n or grid[x + 1, y] == 1:
-        valid_actions.remove(Action.SOUTH)
-    if y - 1 < 0 or grid[x, y - 1] == 1:
+    if x - 1 < 0 or y - 1 < 0 or grid[x-1, y-1] == 1:
+        valid_actions.remove(Action.NORTH_WEST)
+    if x - 1 < 0 or y + 1 > m or grid[x-1, y+1] == 1:
+        valid_actions.remove(Action.NORTH_EAST)
+        
+    if y - 1 < 0 or grid[x, y-1] == 1:
         valid_actions.remove(Action.WEST)
-    if y + 1 > m or grid[x, y + 1] == 1:
+    if y + 1 > m or grid[x, y+1] == 1:
         valid_actions.remove(Action.EAST)
-
+        
+    if x + 1 > n or grid[x+1, y] == 1:
+        valid_actions.remove(Action.SOUTH)
+    if x + 1 > n or y - 1 < 0 or grid[x+1, y-1] == 1:
+        valid_actions.remove(Action.SOUTH_WEST)
+    if x + 1 > n or y + 1 > m or grid[x+1, y+1] == 1:
+        valid_actions.remove(Action.SOUTH_EAST)
     return valid_actions
 
 
@@ -139,8 +154,10 @@ def a_star(grid, h, start, goal):
         print('**********************') 
     return path[::-1], path_cost
 
-
-
 def heuristic(position, goal_position):
-    return np.linalg.norm(np.array(position) - np.array(goal_position))
+    return euclidean(position, goal_position)
+    # return np.linalg.norm(np.array(position) - np.array(goal_position))
+
+def euclidean(p1, p2):
+    return np.sqrt((p2[0] - p1[0])**2 + (p2[1] - p1[1])**2)
 
